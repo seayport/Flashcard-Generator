640045b6d3624d65e32b5e0a7f3344277ac2108c
Sandy Seay <seaysandra@bellsouth.net>
2017-04-14 22:23:17 -0400

    final push

--- Flashcard.js
+++ Flashcard.js
@@ -1,110 +1,110 @@
-var inquirer = require('inquirer');
-// core node package for reading and writing files
-var fs = require('fs');
-var request = require('request');
+var FlashCard = require('./FlashCard');
+var fs = require("fs");
+var inquirer = require("inquirer");
+var request = require("request");
 
-// --------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------
 // Create a BasicCard constructor. It should accept front and back arguments.
-// constructor(parameters)
+// constructor parameters
 //property = value
 function BasicCard(front, back) {
-    if (!this instanceof BasicCard) {
-        this.front = front;
-        this.back = back;
-
-    } else {
-        return new BasicCard(front, back);
-    }
-}
+    // if (!this instanceof BasicCard) {
+    //     return new BasicCard(front, back);
+    // }
+    this.front = front;
+    this.back = back;
+};
 
 BasicCard.prototype.printBasicCard = function() {
     console.log("Question: " + this.front + "\nAnswer: " + this.back);
 };
 
-// ClozeCard Constructor-----------------------------------------------------------------------------------------------------------------------
-function ClozeCard(fulltext, cloze) {
-    if (!this instanceof ClozeCard) {
-        this.partial = text.replace(cloze, "_____")
-        this.cloze = cloze;
-        this.fulltext = fulltext;
-    }
-    return new ClozeCard(fulltext, cloze);
-}
-//ClozeCard prototypes--------------------------------------------------------------------------------------------------------------
-ClozeCard.prototype.printPartialCard = function() {
-    console.log("Partial sentence: " + this.partial);
-}
-ClozeCard.prototype.printClozeCard = function() {
-    console.log("Cloze: " + this.cloze);
-}
-ClozeCard.prototype.printFullCard = function() {
-        console.log("Full sentence: " + this.fulltext);
-        // End of constructors_______________________________________________________________________________________________________________________________
+// ClozeCard Constructor--------------------------------------------------------------
+function ClozeCard(text, cloze) {
+            if (!this instanceof ClozeCard) {
+                 return new ClozeCard(text, cloze);
+            }
+            this.fulltext = text;
+            this.cloze = cloze;
+            this.partial = text.replace(cloze, "_____");      
+        }
+
+//ClozeCard prototypes----------------------------------------------------------------
+        ClozeCard.prototype.printPartialCard = function() {
+            console.log("Partial sentence: " + this.partial);
+        }
+        ClozeCard.prototype.printClozeCard = function() {
+            console.log("Cloze: " + this.cloze);
+        }
+        ClozeCard.prototype.printFullCard = function() {
+            console.log("Full sentence: " + this.fulltext);
+        }
+        // End of constructors_____________________________________________________
         askUser();
         function askUser() {
             inquirer.prompt([{
-                name: "actionType",
-                type: "list",
-                message: "Would you like to create a flashcard or play a game?",
-                choices: ["Create a Basic flashcard", "Create a Cloze flashcard", "Play the flashcard game", "I am done! Log out."],
-                default: ["Play the flashcard game"]
-            }]).then(function(action) {
-                var actionType = action.actionType;
-                switch (actionType) {
-                    case "Create a Basic flashcard":
-                        createBasic();
-                        break;
-
-                    case "Create a Cloze flashcard":
-                        createCloze();
-                        break;
-
-                    case "Play the flashcard game":
-                        play();
-                        break;
-
-                }
+                name: "questionType",
+                type: "rawlist",
+                message: "Would you like to create a basic or cloze flashcard?",
+                choices: ["Basic", "Cloze", "I am done! Log out."]
+            }]).then(function(question) {
+                var questionType = question.questionType;
+                switch (questionType) {
+                    case "Basic":
+                        {
+                            promptBasic();
+                            break;
+                        }
+                    case "Cloze":
+                        {   promptCloze();
+                            break;
+                        }
+                    }
             })
-        }
+        };
 
-        function createBasic() {
+        function promptBasic() {
             inquirer.prompt([{
-                    type: "input",
-                    message: "Enter a question: ",
-                    name: "front"
-                }, {
-                    type: "input",
-                    message: "What is the answer? ",
-                    name: "back"
+                name: "front",
+                type: "input",
+                message: "Enter a question: "   
+            }, {
+                name: "back",
+                type: "input",
+                message: "What is the answer? "
             }]).then(function(basic) {
-                var BasicCard = new BasicCard(basic.front, basic.back);
+                var basicFlashCard = new BasicCard(basic.front, basic.back);
                 console.log("_________________________________________________");
                 console.log("Here's your new Basic flashcard!");
-                fs.appendFile("basicCards.json", JSON.stringify(BasicCard) + '\n', function(err) {
+                basicFlashCard.printBasicCard();
+                fs.appendFile("basicCards.json", JSON.stringify(basicFlashCard) + '\n', function(err) {
+                   if (err) throw err;
+                   askUser();
+                });
+            });
+        }
+
+        function promptCloze() {
+            inquirer.prompt([{
+                name: "text",
+                type: "input",
+                message: "Enter a statement: "
+            }, {
+                name: "cloze",
+                type: "input",
+                message: "Enter cloze: "
+            }]).then(function(cloze) {
+                var ClozeFlashCard = new ClozeCard(cloze.text, cloze.cloze);
+                console.log("_________________________________________________");
+                console.log("Here's your new Cloze flashcard!");
+                ClozeFlashCard.printPartialCard();
+                ClozeFlashCard.printClozeCard();
+                ClozeFlashCard.printFullCard();
+                fs.appendFile("clozeCards.json", JSON.stringify(ClozeFlashCard) + '\n', function(err) {
                     if (err) throw err;
                     askUser();
                 });
             });
         }
 
-    //     function createCloze() {
-    //         inquirer.prompt([{
-    //             name: "fulltext",
-    //             type: "input",
-    //             message: "Enter a true statement: "
-    //         }, {
-    //             name: "cloze",
-    //             type: "input",
-    //             message: "Cloze: "
-    //         }]).then(function(cloze) {
-    //             var ClozeCard = new ClozeCard(cloze.partial, cloze.cloze);
-    //             console.log("_________________________________________________");
-    //             console.log("Here's your new Cloze flashcard!");
-    //             ClozeCard.printPartialCard();
-    //             ClozeCard.printClozeCard();
-    //             ClozeCard.printFullCard();
-    //             fs.appendFile("clozeCards.json", JSON.stringify(ClozeCard) + '\n', function(err) {
-    //                 if (err) throw err;
-    //          });
-    //      });
-    // }({
+        
